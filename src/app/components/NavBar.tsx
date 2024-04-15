18d549f0c806f80ad275c427d40f23f67e30a34b
surajshrestha8 <surajshre348@gmail.com>
2024-04-15 10:04:05 +0545

    updated the phone number

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -57,7 +57,7 @@ const NavBar = () => {
           type="button"
           className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          +977 9814124516
+          014972434
         </button>
       </div>
       <FontAwesomeIcon
